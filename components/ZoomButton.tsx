58147c4b37ce5e7d5c0ad4bd081958b008db54b9
j <115390541+rosaceaee@users.noreply.github.com>
2026-03-27 20:30:36 +0900

    style: 글자 줌 버튼 사이즈 수정

--- components/ZoomButton.tsx
+++ components/ZoomButton.tsx
@@ -1,3 +1,4 @@
+import { scale } from "@/app/utills/scale";
 import { useFontSize } from "@/context/FontSizeContext";
 import { Image, StyleSheet, TouchableOpacity } from "react-native";
 
@@ -16,20 +17,17 @@ export default function ZoomButton() {
 
 const styles = StyleSheet.create({
   button: {
-    position: "absolute",
-    top: 0,
-    right: 0,
-    width: 60,
-    height: 60,
-    borderRadius: 28,
+    width: scale(35),
+    height: scale(35),
+    borderRadius: 24,
     // backgroundColor: Colors.brand.primary,
     alignItems: "center",
     justifyContent: "center",
     zIndex: 996,
   },
   icon: {
-    width: 45,
-    height: 45,
+    width: scale(35),
+    height: scale(35),
     resizeMode: "contain",
   },
 });
